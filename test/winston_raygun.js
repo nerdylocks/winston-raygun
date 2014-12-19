18f8ecdadca2f46f0d93f962087195b769c92d0b
Abiy Seifu <abiy@ripple.com>
2014-12-18 18:36:10 -0800

    [TEST] accept infinite amount of arguments

--- test/winston_raygun.js
+++ test/winston_raygun.js
@@ -1,20 +1,21 @@
-var chai = require('chai');
+var chai    = require('chai');
 var winston = require('winston');
-var Raygun = require(__dirname+'/../lib/winston_raygun.js');
+var Raygun  = require(__dirname+'/../lib/winston_raygun.js');
+var key     = require(__dirname+'/../config/config.json').apiKey;
 
 describe('Winston Raygun transport', function(){
   var raygun;
-  before(function(){
-    raygun = new Raygun({ apiKey: 'test-key' });
-    winston.add(Raygun, { apiKey: 'test-key' });
-  });
 
-  it('should be instance of winston.Transport', function(){
-    chai.assert(raygun instanceof winston.Transport);
+  before(function(){
+    winston.add(Raygun, { apiKey: key });
   });
 
   it('should log add Raygun transport to winston.Transport', function(){
     chai.assert(winston.transports.hasOwnProperty('Raygun'));
   });
 
+  it('should log multiple error object', function() {
+    var error_object = { error_type: 'invalid_request', message: 'error message?'}
+    winston.error('TEST ERROR!', error_object, 'Error', new Error('another'), 'string', false, 9);
+  })
 });
