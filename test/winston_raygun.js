745af2a3067c82a3ce9dcb51921948dfd7da90d1
Paul Cothenet <pcothenet@gmail.com>
2015-08-02 17:31:49 -0600

    Upgrade Raygun and Winston

--- test/winston_raygun.js
+++ test/winston_raygun.js
@@ -1,25 +1,30 @@
 var chai    = require('chai');
 var winston = require('winston');
-var Raygun  = require(__dirname+'/../lib/winston_raygun.js');
+var Raygun  = require(__dirname + '/../lib/winston_raygun.js');
 
 describe('Winston Raygun transport', function(){
 
-  before(function(){
-    winston.add(Raygun, { apiKey: 'test_api_key' });
-  });
+	before(function(){
+		winston.add(Raygun, { apiKey: 'test_api_key' });
+	});
 
-  it('should log add Raygun transport to winston.Transport', function(){
-    chai.assert(winston.transports.hasOwnProperty('Raygun'));
-  });
+	it('should add Raygun transport to winston.Transport', function(){
+		chai.assert(winston.transports.hasOwnProperty('Raygun'));
+	});
 
-  it('should log multiple error object', function() {
-    var error_object = { error_type: 'invalid_request', message: 'error message?'}
-    winston.error('TEST ERROR!', error_object, 'Error', new Error('another'), 'string', false, 9);
-  })
+	it('should log multiple error object', function(done) {
+		var error_object = { error_type: 'invalid_request', message: 'error message?'};
+		winston.error('TEST ERROR!', error_object, 'Error', new Error('another'), 'string', false, 9, function (err) {
+				if (err) {
+					done(err);
+				}
+				done();
+			});
+	});
 
-  it('allows passing in custom Raygun clients', function(){
-    var raygunClient = { name: 'test raygunClient' };
-    var transport = new Raygun({ raygunClient: raygunClient })
-    chai.assert(transport.raygunClient === raygunClient);
-  });
+	it('allows passing in custom Raygun clients', function(){
+		var raygunClient = { name: 'test raygunClient' };
+		var transport = new Raygun({ raygunClient: raygunClient })
+		chai.assert(transport.raygunClient === raygunClient);
+	});
 });
